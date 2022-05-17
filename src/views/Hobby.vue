9c8000550b11c78822cd19a41c0513fc1de600a6
Ko-Ishibashi <mnbvcxz.fur@gmail.com>
2022-05-17 17:05:23 +0900

    Hobby.vueを更新

--- src/views/Hobby.vue
+++ src/views/Hobby.vue
@@ -3,11 +3,12 @@
     <v-row class="section__title , mb-8">
         <v-col class="section__title-text">Hobby</v-col>
     </v-row>
-    <v-carousel
-      cycle
-      show-arrows-on-hover
-      class="mb-8"
+    <v-row
+      align-content="center"
+      justify="center"
     >
+    <v-col cols="12" md="6" lg="6" xl="6">
+    <v-carousel cycle hide-delimiters show-arrows-on-hover class="mb-8">
       <v-carousel-item
         v-for="(item,i) in items"
         :key="i"
@@ -15,7 +16,7 @@
       >
         <v-row
           class="fill-height , white--text"
-          align="center"
+          align="end"
           justify="center"
           height="auto"
         >
@@ -25,11 +26,41 @@
         </v-row>
       </v-carousel-item>
     </v-carousel>
+    </v-col>
+    <v-col cols="12" md="4" lg="4" xl="4" align="center">
+      <div class="hobby__contents-title"><v-icon color="#22577E">mdi-fish</v-icon>&nbsp;Diving</div>
+      <div class="hobby__contents-text">1aaaaaaaaaaaaaaaaaaaa</div>
+      <div class="hobby__contents-title"><v-icon color="#22577E">mdi-palm-tree</v-icon>&nbsp;Trip</div>
+      <div class="hobby__contents-title"><v-icon color="#22577E">mdi-music</v-icon>&nbsp;Drums</div>
+      <div class="hobby__contents-title"><v-icon color="#22577E">mdi-dog</v-icon>&nbsp;Dog</div>
+    </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <style lang="scss" scoped>
 @import '../styles/common/_parts.scss';
+
+.hobby__contents {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  justify-content: center;
+
+  &-title{
+    text-align: center;
+    font-size: 25px;
+    font-weight: bold;
+    margin-bottom: 15px;
+    color: #22577E;
+  }
+
+  &-text {
+    font-size: 15px;
+    margin-top: 15px;
+    margin-bottom: 35px;
+    text-align: center;
+  }
+}
 </style>
 
 <script>
